cb44a105463476492e280b3e0e68ec436673bb14
francisco cotugno <franciscocotugno@gmail.com>
2023-10-02 20:41:15 -0300

    Nuevas carpetas con formato mvc

---
+++ app/controllers/productController.php
@@ -0,0 +1,18 @@
+<?php
+require_once './app/models/productModel.php';
+require_once './app/views/productView.php';
+
+class controllerTask{
+    private $model;
+    private $view;
+
+    public function __construct(){
+        $this->model= new productModel();
+        $this->view= new productView();
+    }
+
+    public function homeController (){
+        require 'templates/header.php';
+        
+    }
+}
