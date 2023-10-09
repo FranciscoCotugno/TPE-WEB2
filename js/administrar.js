94d5c941188ae60dd214f77cbc81b1d9e9152c76
Agustín Gaviña <agusgavina2@hotmail.com>
2023-10-08 22:51:14 -0300

    Estilos a form admin y arreglo de errores

--- js/administrar.js
+++ js/administrar.js
@@ -1,7 +1,8 @@
 "use strict"
 
-const estadoForm = document.querySelector("#estado-form");
+const estadoForm = document.querySelector("#form-title");
 const selectIds = document.querySelector("#select-ids");
+const formAdmin = document.querySelector("#form-administrar");
 const btn = document.querySelector("#btn-form-administrar");
 let var1 = 0;
 
@@ -9,14 +10,17 @@ btn.addEventListener('click', cambiarForm);
 
 function cambiarForm(){
     if (var1 == 0) {
+        formAdmin.setAttribute('action', 'editarProducto')
         estadoForm.innerHTML = 'Editar Productos';
         btn.value = "Agregar"
-        selectIds.className
+        selectIds.className = "form-select__conteiner select-width";
         var1++;
     }
     else if (var1 == 1){
+        formAdmin.setAttribute('action', 'editarProducto')
         estadoForm.innerHTML = 'Agregar Productos';
         btn.value = "Editar"
         var1--;
+        selectIds.className = "noMostrar";
     }
 }
