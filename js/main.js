4293dc6927445241a2020adaa546e0b4f0e5d4f1
Agustín Gaviña <agusgavina2@hotmail.com>
2023-10-09 08:00:23 -0300

    bucle while para imprimir nombres de categorias

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 const btnNav = document.querySelector("#btn-nav");
 const nav = document.querySelector("#nav-links");
-let arregloCategorias = document.querySelectorAll('.categorys');
 
 btnNav.addEventListener("click", menu);
 
@@ -10,19 +9,3 @@ function menu() {
     btnNav.classList.toggle("btn-rotation");
     nav.classList.toggle("nav-open");
 }
-
-function ponerCategoria(){
-    for(let i = 0; i < arregloCategorias.length; i++){
-        let texto = arregloCategorias[i];
-        switch(texto.getAttribute('categoria')){
-            case '1': texto.innerHTML = "Whisky"; break;
-            case '2': texto.innerHTML = "Cerveza"; break;
-            case '3': texto.innerHTML = "Gaseosa"; break;
-            case '4': texto.innerHTML = "Gin"; break;
-            case '5': texto.innerHTML = "Vodka"; break;
-            case '6': texto.innerHTML = "Fernet"; break;
-        }
-    }
-}
-
-ponerCategoria();
